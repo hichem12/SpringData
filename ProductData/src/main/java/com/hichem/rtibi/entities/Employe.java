d0796de40fa608597d3269fed66f3d372ca2ec6c
HichemRtibi <hichem..rtibitalys123@gmail.com>
2019-09-23 15:34:58 +0100

    utilisation table generateur
    create table id_gen(
    gen_name varchar(60) PRIMARY KEY,
    gen_val int(20)
    )

--- ProductData/src/main/java/com/hichem/rtibi/entities/Employe.java
+++ ProductData/src/main/java/com/hichem/rtibi/entities/Employe.java
@@ -4,11 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.TableGenerator;
 
 @Entity
 public class Employe {
+	@TableGenerator(allocationSize = 100,pkColumnName = "gen_name",valueColumnName = "gen_val",name = "employe_gen")
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY,generator = "employe_gen")
 	private int id;
 	private String name;
 	public int getId() {
