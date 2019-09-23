0f979ca89d6b409ffda3b190f9a78ad52041fde5
HichemRtibi <hichem..rtibitalys123@gmail.com>
2019-09-23 13:40:57 +0100

    test create product

--- ProductData/src/test/java/com/hichem/rtibi/ProductDataApplicationTests.java
+++ ProductData/src/test/java/com/hichem/rtibi/ProductDataApplicationTests.java
@@ -2,15 +2,30 @@ package com.hichem.rtibi;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hichem.rtibi.entities.Product;
+import com.hichem.rtibi.repositories.ProductRepos;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductDataApplicationTests {
+	@Autowired
+	private ProductRepos productRepos;
 
 	@Test
 	public void contextLoads() {
+	}
+	@Test
+	public void testCreate() {
+		Product product=new Product();
+		product.setId(1);
+		product.setName("iPhone");
+		product.setDesc("awesome");
+		product.setPrice(1000d);
+		productRepos.save(product);
 	}
 
 }
