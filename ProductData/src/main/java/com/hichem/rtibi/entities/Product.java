ee42bf5d9a8e38959c4fdc59b289b0f90651a095
HichemRtibi <hichem..rtibitalys123@gmail.com>
2019-09-23 12:01:23 +0100

    first Commit

---
+++ ProductData/src/main/java/com/hichem/rtibi/entities/Product.java
@@ -0,0 +1,12 @@
+package com.hichem.rtibi.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Product {
+	private int id;
+	private String name;
+	private String desc;
+	private Double price;
+
+}
