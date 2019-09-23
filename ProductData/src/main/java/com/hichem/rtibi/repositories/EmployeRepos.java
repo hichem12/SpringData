8d92e8d447767f91f3f7f4083ac8ca90152ca520
HichemRtibi <hichem..rtibitalys123@gmail.com>
2019-09-23 15:14:35 +0100

    create employe Repos

---
+++ ProductData/src/main/java/com/hichem/rtibi/repositories/EmployeRepos.java
@@ -0,0 +1,9 @@
+package com.hichem.rtibi.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hichem.rtibi.entities.Employe;
+
+public interface EmployeRepos extends CrudRepository<Employe, Integer>{
+
+}
